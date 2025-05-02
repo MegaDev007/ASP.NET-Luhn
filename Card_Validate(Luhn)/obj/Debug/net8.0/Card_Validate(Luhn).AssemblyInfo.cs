42c7446955c372c8f0990d0badb7c425f1016b25
MegaDev <byddev804@gmail.com>
2025-05-02 07:33:30 -0400

    refactor: remove default weather forecast files

--- Card_Validate(Luhn)/obj/Debug/net8.0/Card_Validate(Luhn).AssemblyInfo.cs
+++ Card_Validate(Luhn)/obj/Debug/net8.0/Card_Validate(Luhn).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Card_Validate(Luhn)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d5382efc0d57a12f1023939105251671a92833")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd965699301fbc1755b74313eb9c151d2b8d174")]
 [assembly: System.Reflection.AssemblyProductAttribute("Card_Validate(Luhn)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Card_Validate(Luhn)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
